5d1038eccc7be1bc4878a11d3c5cc71b133bc80f
businan <yorulmazburaksinan@gmail.com>
2020-09-21 06:10:46 +0200

    finished the project

--- myScript.js
+++ myScript.js
@@ -2,23 +2,37 @@ let myNumber;
 let numberOfGuesses = 0;
 
 function loadNumber() {
-   myNumber = Math.floor(Math.random() * 5) + 1;
-   alert(myNumber)
+    myNumber = Math.floor(Math.random() * 100) + 1;
+    // alert(myNumber)
 }
 
 function check() {
+
     numberOfGuesses++;
     let guess = document.getElementById("myGuess").value;
-    
-    if (parseInt(guess) === parseInt(myNumber)) {
 
-        alert("You are the winner ! You guest after " + numberOfGuesses + " times!");
-        document.location.reload();
-    } else if (parseInt(guess) > parseInt(myNumber)){
+    if (parseInt(guess) === parseInt(myNumber)) {
+        BackGroundChange();
+        //document.getElementById("rabbit").style.visibility = "visible";
+        setTimeout(function() {
+            alert("You are the winner ! You guest after " + numberOfGuesses + " times!");
+        },20);
+        
+        setTimeout(function() {
+            if (confirm("You wanna play again?")) {
+                document.location.reload();
+            } else {
+                window.close();
+            }
+        },30)
+        
+        
+
+    } else if (parseInt(guess) > parseInt(myNumber)) {
         let reaction = "I am thinking about lower number";
         document.getElementById("reaction").innerHTML = reaction;
-        
-    }else {
+
+    } else {
         let reaction = "I am thinking about higher number";
         document.getElementById("reaction").innerHTML = reaction;
     }
@@ -28,3 +42,14 @@ function check() {
 
 
 loadNumber();
+
+function BackGroundChange() {
+    let url = "./image/mix.png";
+
+    let div = document.getElementById("container");
+    div.style.backgroundImage = `url(${url})`;
+    div.style.width = "100%";
+    div.style.height = "100%";
+    return true;
+
+}
